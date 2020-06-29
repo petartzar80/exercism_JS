6bb4cdf1f9b16455e3c1a3eaa76bb7de4b9fe9cf
tzar <petartzar80@gmail.com>
2020-06-29 15:21:28 +0200

    added resistor-color

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,4 @@
+export const colorCode = color => COLORS.indexOf(color);
+
+// prettier-ignore
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
