27af15c58367605d5a7e8b6abb85daa278167f7a
tzar <petartzar80@gmail.com>
2020-07-01 07:03:17 +0200

    refactored resistor-color-duo

--- mentored_track/04_resistor-color-duo/resistor-color-duo.js
+++ mentored_track/04_resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,7 @@
-export const decodedValue = bands => {
-  // prettier-ignore
-  const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
-  return parseInt(
-    COLORS.indexOf(bands[0]).toString() + COLORS.indexOf(bands[1].toString())
-  );
+import { colorCode } from '../03_resistor-color/resistor-color';
+
+export const decodedValue = ([color1, color2]) => {
+  const code1 = colorCode(color1);
+  const code2 = colorCode(color2);
+  return code1 * 10 + code2;
 };
