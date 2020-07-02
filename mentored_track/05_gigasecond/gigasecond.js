24d3bf8864c13189f099aa1021ecafd7a793ec1a
tzar <petartzar80@gmail.com>
2020-07-02 10:57:57 +0200

    refactored gigasecond

--- mentored_track/05_gigasecond/gigasecond.js
+++ mentored_track/05_gigasecond/gigasecond.js
@@ -1,1 +1,2 @@
-export const gigasecond = date => new Date(Date.parse(date) + 1000000000000);
+export const gigasecond = date => new Date(date.getTime() + GIGASEC_IN_MS);
+const GIGASEC_IN_MS = 1e12;
