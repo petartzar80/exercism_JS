009b583ca811bad85fae1658258f463f66db6ba2
tzar <petartzar80@gmail.com>
2020-07-04 09:21:35 +0200

    added pangram

---
+++ mentored_track/08_pangram/pangram.js
@@ -0,0 +1,6 @@
+export const isPangram = sentence =>
+  ALPHABET.every(letter => [...sentence.toLowerCase()].includes(letter));
+
+//prettier-ignore
+const ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+ 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
